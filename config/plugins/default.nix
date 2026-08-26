508df4d8c0124369dc1de34413a600fd7cb63906
Kyle Ross <github@kylejamesross.com>
2026-08-26 13:17:47 -0600

    Remove tinted plugin

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,30 +1,28 @@
 {...}: {
   imports = [
-  ./blink-cmp
-  ./colorizer
-  ./conform
-  ./dap
-  ./gitsigns
-  ./indent-blankline
-  ./lint
-  ./lsp
-  ./mini
-  ./neotest
-  ./telescope
-  ./tinted
-  ./treesitter
-  ./which-key
+    ./blink-cmp
+    ./colorizer
+    ./conform
+    ./dap
+    ./gitsigns
+    ./indent-blankline
+    ./lint
+    ./lsp
+    ./mini
+    ./neotest
+    ./telescope
+    ./treesitter
+    ./which-key
   ];
 
   plugins = {
-      guess-indent.enable = true;
-      illuminate.enable = true;
-      rainbow-delimiters.enable = true;
-      ts-autotag.enable = true;
-      markdown-preview.enable = true;
-      web-devicons.enable = true;
-      otter.enable = true;
-      nvim-autopairs.enable = true;
+    guess-indent.enable = true;
+    illuminate.enable = true;
+    rainbow-delimiters.enable = true;
+    ts-autotag.enable = true;
+    markdown-preview.enable = true;
+    web-devicons.enable = true;
+    otter.enable = true;
+    nvim-autopairs.enable = true;
   };
-
 }
