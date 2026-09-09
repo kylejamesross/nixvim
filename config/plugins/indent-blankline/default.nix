e9075fd28d6919ad8e3006b0f7a634288895a7d5
Kyle Ross <github@kylejamesross.com>
2026-09-09 15:54:33 -0600

    Add transparency to indent blankline colors

--- config/plugins/indent-blankline/default.nix
+++ config/plugins/indent-blankline/default.nix
@@ -1,15 +1,24 @@
 {
-
   plugins.indent-blankline = {
-      enable = true;
-      settings = {
-        indent.char = "┊";
-        scope = {
-          enabled = true;
-          show_start = true;
-          show_end = true;
-        };
+    enable = true;
+    settings = {
+      indent = {
+        char = "┊";
+        highlight = "IblIndent";
       };
+    };
   };
 
+  extraConfigLua = ''
+    local function set_ibl_transparent()
+    	local indent_fg = vim.api.nvim_get_hl(0, { name = "Whitespace", link = false }).fg
+    	vim.api.nvim_set_hl(0, "IblIndent", { fg = indent_fg, bg = "NONE" })
+    end
+
+    set_ibl_transparent()
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+    	callback = set_ibl_transparent,
+    })
+  '';
 }
