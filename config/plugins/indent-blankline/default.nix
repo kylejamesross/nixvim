cda4e1e48f084e18471e031b8735be5d2edabe05
Kyle Ross <github@kylejamesross.com>
2026-09-09 15:57:40 -0600

    Revert did not work

--- config/plugins/indent-blankline/default.nix
+++ config/plugins/indent-blankline/default.nix
@@ -8,17 +8,4 @@
       };
     };
   };
-
-  extraConfigLua = ''
-    local function set_ibl_transparent()
-    	local indent_fg = vim.api.nvim_get_hl(0, { name = "Whitespace", link = false }).fg
-    	vim.api.nvim_set_hl(0, "IblIndent", { fg = indent_fg, bg = "NONE" })
-    end
-
-    set_ibl_transparent()
-
-    vim.api.nvim_create_autocmd("ColorScheme", {
-    	callback = set_ibl_transparent,
-    })
-  '';
 }
